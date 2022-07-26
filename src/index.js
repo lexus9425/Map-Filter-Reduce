4dfbe8fd7053cb6bf15230ad27beb6996bb5844a
lexus9425 <68475116+lexus9425@users.noreply.github.com>
2022-07-26 14:55:55 -0400

    Final Commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
-var numbers = [3, 56, 2, 48, 5];
+//var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
+//function double(x) {
+//  return x * 2;
+//}
+
+//const newNumbers = numbers.map(double);
+
+//console.log(newNumbers);
+
 //Filter - Create a new array by keeping the items that return true.
+
+//const newNumbers = numbers.filter(function (num) {
+//  return num > 10;
+//});
+
+//console.log(newNumbers);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
 //Find - find the first item that matches from an array.
 
+//const newNumber = numbers.find(function (num) {
+//  return num > 10;
+//});
+
+//console.log(newNumber);
+
 //FindIndex - find the index of the first item that matches.
+
+//const newNumber = numbers.findIndex(function (num) {
+//  return num > 10;
+//});
+
+//console.log(newNumber);
+
+//CHALLENGE
+import emojipedia from "./emojipedia";
+
+const newEmojipedia = emojipedia.map(function (emojiEntry) {
+  return emojiEntry.meaning.substring(0, 100);
+});
+
+console.log(newEmojipedia);
